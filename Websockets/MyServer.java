d8e7c7259579afc91a47ea8fcce9027eacd69a83
Javier <mscjaviergs@gmail.com>
2020-12-16 01:28:09 -0700

    Update MyServer.java

--- Websockets/MyServer.java
+++ Websockets/MyServer.java
@@ -2,18 +2,18 @@ import org.glassfish.tyrus.server.Server;
 
 public class MyServer {
 	
-	public static void main (String[] args) {
-		Server server;
-		server = new Server ("localhost", 8025, "/project", MyServerEndpoint.class);
-		try {
-			server.start();
-			System.out.println("--- server is running");
-		  while(true){}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			server.stop();
-		}
-	}
+  public static void main (String[] args) {
+    Server server;
+    server = new Server ("localhost", 8025, "/project", MyServerEndpoint.class);
+    try {
+      server.start();
+      System.out.println("--- server is running");
+      while(true){}
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      server.stop();
+    }
+  }
 	
 }
