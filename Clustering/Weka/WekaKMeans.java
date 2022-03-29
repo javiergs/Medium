b227433421e660367cf14fb0a0d3e30d852732a3
Javier <mscjaviergs@gmail.com>
2022-03-29 10:06:49 -0700

    Update WekaKMeans.java

--- Clustering/Weka/WekaKMeans.java
+++ Clustering/Weka/WekaKMeans.java
@@ -1,20 +1,35 @@
+import weka.clusterers.ClusterEvaluation;
+import weka.clusterers.SimpleKMeans;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-import weka.clusterers.SimpleKMeans;
 
 public class WekaKMeans {
 
   public static void main(String[] args) {
     try {
+      // clustering
       DataSource src = new DataSource("weather.arff");
       Instances dt = src.getDataSet();
       SimpleKMeans model = new SimpleKMeans();
       model.setNumClusters(3);
+      model.setPreserveInstancesOrder(true);
       model.buildClusterer(dt);
-      System.out.println(model);
-
+      // show labels per record
+      int[] assignments = model.getAssignments();
+      int i = 0;
+      for (int clusterNum : assignments) {
+        System.out.printf("Instance %d [%s] -> Cluster %d \n", i, dt.get(i), clusterNum);
+        i++;
+      }
+      // evaluation
+      ClusterEvaluation eval = new ClusterEvaluation();
+      eval.setClusterer(model);
+      eval.evaluateClusterer(dt); // yes, we need to use a test dataset here :)
+      System.out.println(eval.clusterResultsToString());
+      System.out.println(eval.getLogLikelihood());
     } catch (Exception e) {
       System.out.println(e.getMessage());
     }
   }
 }
+
